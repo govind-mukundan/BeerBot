9a680fd306cd72d1c7721a672299f939c0cf803c
Govind Mukundan <govind.mukundan@gmail.com>
2013-02-12 02:02:35 +0800

    Tested with epoch time stamp and auth code format #AC:000001&. Included vending with auth code stored in EEPROM

--- sketch_sm5100_sms.ino
+++ sketch_sm5100_sms.ino
@@ -1,6 +1,16 @@
 /*
-Hardware Notes:
- --> Power supply should be at least 5V, 2.5Ampere and must be given to the GSM Shield, NOT to the Arduino
+
+                      Description: INO for beerbot task #2 in Basecamp.
+                      Hardware: Arduino Leonardo
+                      Author: Govind Mukundan (govind.mukundan at gmail.com)
+                      Date: 12/Feb/2013
+                      Version: 1.1
+                      References: Basic intro to SM5100 -> http://tronixstuff.wordpress.com/tag/sm5100/
+                      History:
+                                1.1: First Version, tested at home without Bot using SINGTEL SIM card
+
+ Hardware Notes:
+ --> Power supply should be at least 5V, 2.5Ampere and must be given to the GSM Shield, NOT to the Arduino power jack.
  --> Ardiono should be powered from the 5V pin on the shield
  --> The Leonardo uses the USB port as the serial port, so pins (0,1) are free to use for the GSM shield (Serial1).
  --> However, the SMB Shield is by default configured to use pins (2,3) for UART, So you have to switch the Rx and Tx Jumpers on the board to use Serial1
@@ -76,13 +86,17 @@ Hardware Notes:
  AT+CMGF=1
  AT+CMGL="ALL"
  
- 
- 
- ToDo:
- 1. Auto Connect and initialize SMS Shield.
- 2. Indicate that ready to receive or Error
- 3. Once authorization SMS is received, vend beer
- 
+---- Sent.ly API -----
+To send an SMS via Sent.ly using HTTP,
+1. Create an account
+2. Register an android phone
+3. Log into the phone app
+4. Use the foll HTTP GET syntax 
+https://sent.ly/command/sendsms?username=sent_ly_uname&password=sent_ly_pwd&to=%2b6596376447&text=%23AC:1360604509%26
+
+--> The sender number will be the android phone runnung sent.ly
+--> %23AC:1360604509%26, 0x23 and 0x26 are # and & in ASCII (URL Encoding) 
+--> Note that the App on the phone MUST be running in order for the SMSes to be sent, if not you will get an error code "ERROR 3" on the browser
  
  http://playground.arduino.cc/Main/Printf
  
@@ -91,10 +105,18 @@ Hardware Notes:
 
 #include <string.h>         //Used for string manipulations
 #include <ctype.h>
+#include <EEPROM.h>
 
 #define SM_BUFFER_LEN  100
 #define DEBUGGING (1)  // make 0 before release
 
+#define C_EEPROM_APPROVALID_ADDRESS            (0x100)
+#define C_EEPROM_APPROVALID_SIZE_ADDRESS       (0x100 + 50u)
+
+#define C_APPROVAL_ID_LEN    (20)
+#define LED_PIN      (8) // On board LED at pin13 (L)
+#define C_ERROR_BLINK_RATE          (1000ul)
+#define C_AUTHCODE_HEADER_BYTES    (3) // AC:
 
 char incoming_char=0;      //Will hold the incoming character from the Serial Port.
 //String inputString = "";         // a string to hold incoming data
@@ -102,6 +124,20 @@ char SMBuffer[SM_BUFFER_LEN];
 char BufferTail;
 char CmdStart = 0;
 boolean CmdComplete = false;  // whether the string is complete
+// EEPROM copy
+byte LastApprovalIdLen;
+char LastApprovalId[C_APPROVAL_ID_LEN];
+
+// Hardware
+int left = 12;
+int right = 13;
+int vendside = 0;
+
+unsigned long blinkTimeStamp = 0;                // Time Stamp
+unsigned long blinkTimePeriod = 0;
+unsigned long errorTimeStamp = 0;
+
+#define C_ERROR_TIMEOUT    (90000) //1.5 min
 
 enum {
   SM_SIM_REMOVED,
@@ -131,24 +167,37 @@ e_SM5100States;
 
 e_SM5100States SM5100State;
 
+void SM5100LoopBack(void); // Useful for testing, to control the shield from PC Terminal. Just call this in the loop(), disable everything else.
+void setBlinkRate(byte value);
+void Blink(void);
+void SM5100RxTask(void) ;
+void SM5100ParseStatus(void);
+void SM5100StateMachine(void);
+void SM500InitializeSMS(void);
+void SM5100ParseSms(void);
+
+
 void setup()
 {
   //Initialize serial ports for communication.
+  pinMode(LED_PIN, OUTPUT);
   Serial.begin(9600);
   Serial1.begin(9600);
-  #if (DEBUGGING == 1)
+#if (DEBUGGING == 1)
   while (!Serial) {
     ; // wait for serial port to connect. Needed for Leonardo only
   }
-  #endif
+#endif
+  loadConfigFromEEPROM();
   Serial.println(F("Starting SM5100B Communication..."));
-
+  blinkTimeStamp = millis();
 }
 
 void loop()
 {
   SM5100RxTask();
   SM5100StateMachine();
+  Blink();
   // print the string when a newline arrives:
   if (CmdComplete) {
     SM5100ParseStatus();
@@ -158,44 +207,57 @@ void loop()
 
 }
 
-void SM5100LoopBack(void)
-{
-  //If a character comes in from the cellular module...
-  if(Serial1.available() >0){
-    incoming_char=Serial1.read();    //Get the character from the cellular serial port.
-    Serial.print(incoming_char);  //Print the incoming character to the terminal.
-  }
-
-  //If a character is coming from the terminal to the Arduino...
-  if(Serial.available() >0){
-    incoming_char=Serial.read();  //Get the character coming from the terminal
-    Serial1.print(incoming_char);    //Send the character to the cellular module.
-  }
-
-}
-
 
 /*
-  SerialEvent occurs whenever a new data comes in the
- hardware serial RX.
+ We are interested in two types of messages:
+ 1. SIND status commands - they are of the form +SIND: xx \r
+ 2. Auth Codes for the Bot - they are of the form #AC:xxxxx&
+ So this task looks for either start bits -> + or # 
+ When it receives either, it starts logging all the next bytes that are not spaces
+ into the buffer. Once the end character - \r or # is received, it stops logging
+ and notifies the application that a command was received.
+ $ and # are selected for the authcode delimiters because the sms shield does not produce 
+ them in normal operation.
+ 
+ When you receive an SMS this is what the Arduino sees:
+ +CMT: "+6596376447","+6596197777","13/02/12,00:36:55+00",11
+ #AC:000001&
+ First you have the sender Phone number, then the SMS gateway number then the date and time
+ Finally the number of characters in the message (11), followed by the message itself on a new line.
  */
-void SM5100RxTask() {
-  while (Serial1.available()) {
+void SM5100RxTask(void) {
+  while (Serial1.available() > 0) {
     // get the new byte:
     char inChar = (char)Serial1.read();
     Serial.print(inChar); // dump it out to the serial port
     // discard spaces and data in between commands
-    if((inChar != ' ')&& (CmdStart == true)){
-      if ((inChar == '\n') || (inChar == '\r')) {
-        CmdComplete = true;
-        CmdStart = false;
+    if(SM5100State != SM5100WForSms){
+      if((inChar != ' ')&& (CmdStart == true)){
+        if ((inChar == '\n') || (inChar == '\r')) {
+          CmdComplete = true;
+          CmdStart = false;
+        }
+        else{
+          SMBuffer[BufferTail++] = inChar; 
+        }
       }
-      else{
-        SMBuffer[BufferTail++] = inChar; 
+      if ( (inChar == '+')) {
+        CmdStart = true;
       }
     }
-    if ( (inChar == '+') || () ) {
-      CmdStart = true;
+    else{
+      if((inChar != ' ')&& (CmdStart == true)){
+        if ( (inChar == '&')) {
+          CmdComplete = true;
+          CmdStart = false;
+        }
+        else{
+          SMBuffer[BufferTail++] = inChar; 
+        }
+      }
+      if ((inChar == '#')) {
+        CmdStart = true;
+      }
     }
 
   }
@@ -207,10 +269,11 @@ void SM5100ParseStatus(void)
 {
   // Parse out SIND status updates
   byte stat = SM_DUMMY;
+  Serial.println(F("Parsing Status..")); 
   if(strncmp(SMBuffer,"SIND:",5)==0) {
-    if( isDigit(SMBuffer[5]) && isDigit(SMBuffer[6]) ) { //(BufferTail >= 7) && (
+    if( isDigit(SMBuffer[5]) && isDigit(SMBuffer[6]) ) {
       char a[3] = {
-        SMBuffer[5],SMBuffer[6],'\0'            };
+        SMBuffer[5],SMBuffer[6],'\0'                              };
       stat = atoi(a);
       SMBuffer[5] = '\0';
       SMBuffer[6] = '\0';
@@ -224,11 +287,12 @@ void SM5100ParseStatus(void)
     Serial.println(F(" "));
   }
 
-  switch(stat)
+  switch(stat) // Change states of SIM module based on network status
   {
   case SM_SIM_REMOVED: 
     Serial.println(F("SM_SIM_REMOVED")); 
     SM5100State = SM5100Error; 
+    errorTimeStamp = millis();
     break;
   case SM_SIM_INSERTED: 
     Serial.println(F("SM_SIM_INSERTED"));  
@@ -252,10 +316,12 @@ void SM5100ParseStatus(void)
   case SM_NETWORK_EMERGENCY: 
     Serial.println(F("SM_NETWORK_EMERGENCY")); 
     SM5100State = SM5100Error; 
+    errorTimeStamp = millis();
     break;
   case SM_NETWORK_LOST: 
     Serial.println(F("SM_NETWORK_LOST")); 
     SM5100State = SM5100Error; 
+    errorTimeStamp = millis();
     break;
   case SM_AUDIO_ON: 
     Serial.println(F("SM_AUDIO_ON")); 
@@ -276,6 +342,7 @@ void SM5100ParseStatus(void)
 
 }
 
+// 
 void SM5100StateMachine(void)
 {
 
@@ -286,19 +353,23 @@ void SM5100StateMachine(void)
     break;
 
   case SM5100Ready:
-   SM500InitializeSMS();
+    SM500InitializeSMS();
     break;
-    
+
   case SM5100WForSms:
-      if (CmdComplete) {
-    SM5100ParseSms();
-    BufferTail = 0;
-    CmdComplete = false;
-  }
+    if (CmdComplete) {
+      SM5100ParseSms();
+      BufferTail = 0;
+      CmdComplete = false;
+    }
     break;
-    
-    case SM5100Error:
-    
+
+  case SM5100Error:
+    if( ((millis() - errorTimeStamp) & 0xFFFFFFFF )>=  C_ERROR_TIMEOUT){
+      SM5100State = SM5100Ready; // Attempt to restart..
+      errorTimeStamp = millis();
+      Serial.println(F("State:ERROR, trying to restart"));
+    }
     break;
 
   }
@@ -313,31 +384,183 @@ void SM500InitializeSMS(void)
   Serial1.println(F("AT+CMGF=1"));
   Serial.setTimeout(10000);
   Serial1.readBytesUntil('\n', SMBuffer, 15);
-    if(strncmp(SMBuffer,"OK",2)==0){
-      // OK from shield 
-      Serial.println(F("Text Mode"));
-      Serial1.flush();
-      Serial1.println(F("AT+CNMI=3,3,0,0"));
-      Serial.println(F("Waiting for SMS"));
-      SM5100State = SM5100WForSms;
-    }
+  if(strncmp(SMBuffer,"OK",2)==0){
+    // OK from shield 
+    Serial.println(F("Text Mode"));
+    Serial1.flush();
+    Serial1.println(F("AT+CNMI=3,3,0,0"));
+    delay(3000); // Let it process the command
+    Serial.println(F("Deleting Messages in SIM"));
+    Serial1.println(F("AT+CMGD=1,4"));
+    Serial.println(F("Waiting for SMS"));
+    SM5100State = SM5100WForSms;
+  }
   // reset sind buffer
   SMBuffer[5] = '\0';
   SMBuffer[6] = '\0';
 }
 
-
-void SM5100ParseSms(void)
+boolean compareArrays(char* array1, char* array2, int length)
 {
-    if(strncmp(SMBuffer[0]=='#') {
-
-    Serial.println(F("Auth="));
-    Serial.print(stat);
-    Serial.println(F(" "));
-  }
-  
+  for(int i = 0; i < length; i++){
+    if(array1[i] != array2[i]){
+      return false;
+    }
+  } 
+  return true;
 }
 
 
+
+void SM5100ParseSms(void)
+{
+  char temp;
+  boolean approval = false;
+  byte authCodeLen = 0;
+  Serial.println(F("Parsing SMS.."));
+  if(strncmp(SMBuffer,"AC:",C_AUTHCODE_HEADER_BYTES)==0) { // Make sure its the auth code
+    // Auth code length can change (increase), so read until BufferTail
+    // Compare values
+    Serial.println(F("New ID ="));
+    Serial.write((const uint8_t*)SMBuffer,BufferTail);
+    Serial.println(F(" "));
+    authCodeLen = BufferTail - C_AUTHCODE_HEADER_BYTES; // Now point to the 
+    if(compareArrays((char*)LastApprovalId,(char*)&SMBuffer[C_AUTHCODE_HEADER_BYTES],authCodeLen) == false)
+      approval = true;// values are different, approve beer
+
+    if(approval){
+      Serial.println(F("Beer Approved!, Vending.."));
+      vendBeer();
+      Serial.println(F("Writing ID into EEPROM"));
+      // Store it into EEPROM
+      writeToEEPROM(C_EEPROM_APPROVALID_ADDRESS, (char*)&SMBuffer[C_AUTHCODE_HEADER_BYTES], authCodeLen);
+      writeToEEPROM(C_EEPROM_APPROVALID_SIZE_ADDRESS, (char*)&authCodeLen, 1);
+      // Copying it into RAM for access in the current power cycle
+      memcpy ( LastApprovalId, &SMBuffer[C_AUTHCODE_HEADER_BYTES], authCodeLen );
+    }
+    else{
+      Serial.println(F("IDs are same, no beer for you"));
+
+    }
+  }
+  else{
+    Serial.println(F("Unknown Message:"));
+    Serial.write((const uint8_t*)SMBuffer,BufferTail); 
+    SM5100State = SM5100Error;
+    setBlinkRate(3); // Status: Error
+  }
+
+}
+
+boolean vendBeer(void)
+{
+
+  vendside = 1 - vendside;
+
+  if (vendside == 1) {
+    digitalWrite(right, HIGH);
+    delay(500); //TODO - change these to 500
+    digitalWrite(right, LOW);
+
+  } 
+  else {
+    digitalWrite(left, HIGH);
+    delay(500); //TODO - change these to 500
+    digitalWrite(left, LOW);
+  }
+  return true;
+
+}
+
+void writeToEEPROM(int address, char* dataP, int len)
+// Writes the data into EEPROM
+// EEPROM write does not return anything..
+{
+  int i;
+  for(i=0;i<len;i++)
+  {
+    EEPROM.write((address+i), *(dataP+i));
+  } 
+}
+
+
+void readFromEEPROM(int address, char* dataP, int len)
+// Reads the data from EEPROM
+{
+  int i;
+  for(i=0;i<len;i++)
+  {
+    *(dataP+i) = EEPROM.read(address+i);
+  } 
+}
+
+void loadConfigFromEEPROM(void)
+// Loads all the configuration informaton from EEPROM To RAM for faster access later
+{
+  char temp;
+  Serial.println(F("Reading cfg from EE"));
+
+
+  Serial.println(F("Reading Last Approval ID from EEPROM"));
+  readFromEEPROM(C_EEPROM_APPROVALID_SIZE_ADDRESS, (char*)&LastApprovalIdLen, 1);
+  if(LastApprovalIdLen <= C_APPROVAL_ID_LEN)
+  {
+    readFromEEPROM( C_EEPROM_APPROVALID_ADDRESS, (char*)&LastApprovalId[0], LastApprovalIdLen ); 
+    Serial.println(F("Old Approval ID:"));
+    Serial.write((uint8_t*)&LastApprovalId[0],LastApprovalIdLen);
+  }
+  else
+  {
+    LastApprovalIdLen = 0; // reset the old length, case when EEPROM is uninitialized
+    Serial.println("Reset Last Approval ID");
+  } 
+
+}
+
+void setBlinkRate(byte value)
+// The WiFi status and connect status cant be indiated by blinking because those functions dont return until success/fail
+{
+  switch(value)
+  {
+
+  case 0:
+    blinkTimePeriod = 0xFFFFFFFF;
+    break;
+
+  case 3:
+    blinkTimePeriod = C_ERROR_BLINK_RATE;
+    break;
+
+  default:
+    break;
+  } 
+
+}
+
+void Blink(void)
+{
+  if( ((millis() - blinkTimeStamp) & 0xFFFFFFFF )>=  blinkTimePeriod){
+    digitalWrite( LED_PIN  , 1 ^ digitalRead(LED_PIN )) ;
+    blinkTimeStamp = millis();
+  }
+
+}
+
+
+void SM5100LoopBack(void)
+{
+  //If a character comes in from the cellular module...
+  if(Serial1.available() >0){
+    incoming_char=Serial1.read();    //Get the character from the cellular serial port.
+    Serial.print(incoming_char);  //Print the incoming character to the terminal.
+  }
+
+  //If a character is coming from the terminal to the Arduino...
+  if(Serial.available() >0){
+    incoming_char=Serial.read();  //Get the character coming from the terminal
+    Serial1.print(incoming_char);    //Send the character to the cellular module.
+  }
+
+}
 
 
